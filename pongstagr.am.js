de9362ed19aeaae8b5e48244d630b6a2dd537914
Pongstr <twiz.tickler@gmail.com>
2013-05-19 11:05:19 +0800

    basic function to display media

--- pongstagr.am.js
+++ pongstagr.am.js
@@ -11,6 +11,16 @@
 ;(function ($, window, document, undefined){
   
   "use strict";
+  
+  function renderHTML( targetElement, request ){
+    var galleryList      = '<ul class="thumbnails"></ul>',
+        galleryContainer = '<div class="row-fluid">' + galleryList + '</div>',
+        paginateBtn      = '<div class="row-fluid"><a href="#" data-paginate="'+ request +'-pages" class="span6 offset3 btn btn-large btn-block btn-success">Load More</a></div>';
+
+    $( targetElement ).append( galleryContainer );  
+    $( targetElement ).after( paginateBtn );  
+  }
+  
 
   function ajaxRequest( endpoint ){
     $.ajax({
@@ -19,21 +29,54 @@
       cache    : true     ,
       dataType : "jsonp"  ,
       success  : function(data){
-        console.log( data );
+        
+        $.each( data.data, function( key, value){
+
+          var thumbnail  = value.images.low_resolution.url, 
+          // ( value.images.standard_resolution.url ) ? value.images.standard_resolution.url : value.images.low_resolution.url,
+              caption    = ( value.caption !== null ) ? ( value.caption.text !== null ) ? value.caption.text : '' : value.user.username,
+              nextUrl    = data.pagination.next_url,
+              // comments   = ( value.comments.count   ) ? value.comments.count : '0',
+              
+              thumbBlock  = '<li class="span3"><div class="thumbnail">';
+              thumbBlock += '<img src="' + thumbnail + '" alt="' + caption + '" />';
+              thumbBlock += '</div></li>';
+              
+          $('.thumbnails').append( thumbBlock );
+          
+        });
+        
+        paginate( data.pagination.next_url );
       }
     });
   }
+
+  function paginate( nextUrl ){
+    if ( nextUrl !== undefined || nextUrl !== null ) {
+      $('.btn').click(function(event){
+        
+        ajaxRequest( nextUrl ); // Load Succeeding Pages.
+        
+       $(this).unbind(event);   // Unbind all attached events.
+      });
+    } else {
+      $('.btn').click(function(){
+        
+      });
+    }
+  }
   
-  function requestData ( request, count, accessID, accessToken ){
+  function requestData ( request, count, accessID, accessToken, targetElement ){
     var $apiRequest   = 'https://api.instagram.com/v1/users/',  
         $requestCount = ( count !== null ) ?  
           '?count=' +  count + '&access_token=' + accessToken :
-          '?count=' +    8   + '&access_token=' + accessToken ;
+          '?count=' +    8   + '&access_token=' + accessToken ,
+        loadBtnData  = ( request === null ) ? 'recent' : request ;
     
     if ( request === null || request === 'recent' ){
       var $recentMedia = $apiRequest + accessID + '/media/recent' + $requestCount; 
           // Load Recent Media
-          ajaxRequest( $recentMedia );
+          ajaxRequest( $recentMedia, request );
     }
     
     if ( request === 'liked' ){
@@ -47,6 +90,7 @@
           // Load User Feed
           ajaxRequest( $feedMedia );
     }
+    renderHTML( targetElement, loadBtnData );
   }
 
   function accessDetails( accessID, accessToken ){
@@ -63,10 +107,11 @@
     // Plugin Options
     var option  = $.extend({}, $.fn.pongstgrm.defaults, options);
     
-    return this.each( function(){
+    return this.each( function(i, element){
       
       if ( accessDetails( option.accessId, option.accessToken ) !== false ){
-        requestData( option.show, option.count, option.accessId, option.accessToken );
+        requestData( option.show, option.count, option.accessId, option.accessToken, element );
+        
       }
       
     });  //*! end return this.each;
