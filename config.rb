deff2cf3f792fdea42b1f8bd0803e14d9730073c
Pongstr <twiz.tickler@gmail.com>
2013-07-15 14:59:40 +0800

    updates.

---
+++ config.rb
@@ -0,0 +1,39 @@
+# =============================================================================
+# Require any additional compass plugins here.
+# =============================================================================
+ 
+# Uncomment if you are using Bootstrap-Sass
+require 'bootstrap-sass' 
+ 
+ 
+ 
+# Set this to the root of your project when deployed:
+# -----------------------------------------------------------------------------
+http_path       = "/"
+css_dir         = "docs/assets/css"
+sass_dir        = "scss"
+images_dir      = "docs/assets/img"
+javascripts_dir = "docs/assets/js"
+ 
+# You can select your preferred output style here 
+# (can be overridden via the command line) possible options are: 
+# [ :expanded or :nested or :compact or :compressed ]
+# -----------------------------------------------------------------------------
+output_style   = :expanded
+ 
+# To enable relative paths to assets via compass helper functions.
+# Boolean: True or False
+# -----------------------------------------------------------------------------
+relative_assets = true
+ 
+# To disable debugging comments that display the original location of your selectors.
+# -----------------------------------------------------------------------------
+line_comments = false
+ 
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# -----------------------------------------------------------------------------
+# preferred_syntax = :sass
+ 
+# and then run this in Terminal:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
