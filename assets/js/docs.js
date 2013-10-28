fc38b19f6f48b0eb1205a7c4c68f06250771e12b
Pongstr <twiz.tickler@gmail.com>
2013-10-28 14:10:48 +0800

    created docs page

--- assets/js/docs.js
+++ assets/js/docs.js
@@ -4,3 +4,35 @@
  * Copyright (c) 2013 Pongstr Ordillo. MIT License
  * Requires: jQuery v1.10.2 and Bootstrap 3.0.0
  * ========================================================================= */
+
+$(window).load(function () { "use strict"; 
+
+  window.prettyPrint && prettyPrint()
+
+  var headerHeight = $('header').outerHeight();
+
+  $(window).scroll( function (){
+    ($(window).scrollTop() > headerHeight) ?
+      $('body, .page-heading').addClass('sticky') :
+      $('body, .page-heading').removeClass('sticky')
+  })
+
+
+  $('[data-hash=slide]').each(function () {
+    $(this).on('click', function (e) {
+      e.preventDefault()
+
+      var  target = $(this).attr('href')
+        , $target = $(target)
+        ,  offset = ($(this).data('hash-offset') == undefined) ? 160 : $(this).data('hash-offset') 
+
+      $('html, body').stop().animate({
+        'scrollTop': $target.offset().top - offset
+        }, { duration: 1250, easing: 'easeInOutExpo'}, function() {
+          window.location.hash = target
+        return false
+      })
+    })
+  })
+
+});
