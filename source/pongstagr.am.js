1ecb141c7a3b93c6b1ec1f9908619b3e1ecff927
Pongstr <twiz.tickler@gmail.com>
2013-11-06 00:16:09 +0800

    disabled custom video controls ftm

--- source/pongstagr.am.js
+++ source/pongstagr.am.js
@@ -130,12 +130,12 @@
             }
 
             if (options.data.type === 'video') {
-                modal += '<video width="100%" height="auto">'
+                modal += '<video autoplay width="100%" height="auto">'
                 modal += '  <source src="'+ options.data.video +'" type="video/mp4">'
                 modal += '</video>'
-                modal += '<div class="video-controls text-center">'
-                modal += '  <button type="button" class="btn btn-default" id="play-pause"><i class="'+ options.dflt.videoicon +'"></i></button>'
-                modal += '</div>'
+                // modal += '<div class="video-controls text-center">'
+                // modal += '  <button type="button" class="btn btn-default" id="play-pause"><i class="'+ options.dflt.videoicon +'"></i></button>'
+                // modal += '</div>'
             }
 
             modal += '</div>'
