44c117cd5e559c8b02f8adc0d9cdf0823888b562
Pongstr <twiz.tickler@gmail.com>
2013-11-08 15:19:58 +0800

    added grunt tasks and minified sources

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! ========================================================================== \n' +
+            ' * jQuery Pongstagr.am v<%= pkg.version %> \n' +
+            ' * ========================================================================== \n' +
+            ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %> \n' +
+            ' * Requires: <%= pkg.requires %> \n' +
+            ' * ========================================================================= */\n\n\n',
+    jqueryCheck: 'if (typeof jQuery === "undefined") { throw new Error("<%= pkg.name %> <%= pkg.requires %>") }',
+    jshint: {
+      options: { jshintrc: 'source/.jshintrc' },
+      gruntfile: { src: 'Gruntfile.js' },
+      src: { src: ['source/*.js' ] }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %><%= jqueryCheck %>',
+        stripBanners: false
+      },
+      bootstrap: { 
+        src: 'source/<%= pkg.name %>.js',
+        dest: 'source-min/<%= pkg.name %>.min.js'
+      }
+    },
+    recess: {
+      dist: {
+        options: { 
+          banner: '<%= banner %>',
+          compress: true,
+          noIDs: true,
+          zeroUnits: true
+        },
+        src:  'source/<%= pkg.name %>.css',
+        dest: 'source-min/<%= pkg.name %>.min.css'
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-recess');
+
+
+  // Compress Sources
+  grunt.registerTask('default', ['jshint','uglify', 'recess']);
+
+};
