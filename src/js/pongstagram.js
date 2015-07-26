4eb860e5474c347286026e5fd57c2a967833240d
Pongstr <twiz.tickler@gmail.com>
2015-07-26 20:14:04 +0800

    updated

--- src/js/pongstagram.js
+++ src/js/pongstagram.js
@@ -1,301 +1,166 @@
-
 +function ($) { 'use strict';
 
-  var Pongstagram = function (element, option) {
-    this.element = element || '';
-    this.options = option;
-    this.ig_api  = 'https://api.instagram.com/v1/'
+  var Pongstagram = function (element, options) {
+    this.element = element || '<div/>'
+    this.options = options || Pongstagram.DEFAULTS
+    this.instgrm = 'https://api.instagram.com/v1'
 
+    this.authenticate()
+
+    console.log(this)
     return this
-  };
+  }
 
-  Pongstagram.VERSION = '0.1.0';
+  Pongstagram.VERSION = '0.1.0'
 
-  // Default Options
-  Pongstagram.defaults = {
+  // Default Plugin Ooptions
+  Pongstagram.DEFAULTS = {
     // user authentication
-      accessId:     null
-    , accessToken:  null
+      accessId:     ''
+    , accessToken:  ''
 
     // display options
-    , count:       8                // integer: default number of media to be displayed initially
+    , count:       1                // number:  default number of media to be displayed
     , likes:       true             // boolean: show/hide likes count
     , comments:    true             // boolean: show/hide comments
     , timestamp:   true             // boolean: show/hide media timestamp
     , show:       'recent'          // string:  possible options are feed, likes, media, profile and tag
 
     // plugin components
-    , btn_like:           true      // boolean: show/hide like button
-    , btn_mute:           true      // boolean: show/hide mute button (for video)
-    , btn_comment:        true      // boolean: show/hide comment field
-    , profile_bg_img:     null      // string:  valid image uri
-    , profile_picture:    64        // number: profile picture dimensions in square format e.g., (128 == 128x128 pixels)
-    , profile_bg_color:   '#d9534f' // string: hex value of profile's background-color
-  };
+    , btn_like:         true      // boolean: show/hide like button
+    , btn_mute:         true      // boolean: show/hide mute button (for video)
+    , btn_comment:      true      // boolean: show/hide comment field
+    , profile_bg_img:   null      // string:  valid image uri
+    , profile_picture:  64        // number:  profile picture dimensions in square format e.g., (128 == 128x128 pixels)
+    , profile_bg_color: '#d9534f' // string:  hex value of profile's background-color
+  }
 
-  Pongstagram.prototype.thumbnail = function (data) {
-    var
-      pongstr = this
-    , options = this.options
-    , markup  = {};
+  Pongstagram.prototype.markup = function () {
 
-    markup.date = $('<span>', {
-      class: 'item-date',
-      text: data.created_time
-    })
-
-    markup.image = $('<img/>', {
-        src: data.images.low_resolution.url
-      , alt: data.caption ? data.caption.text : ''
-      , class: 'p-container-thumbnail'
-    }).lazyload()
-
-    markup.likes = $('<span/>', {
-      class: 'item-like',
-      text: data.likes.count
-    })
-
-    markup.comments = $('<span/>', {
-      class: 'item-comments',
-      text: data.comments.count
-    })
-
-    markup.stats = $('<div/>')
-      .addClass('item-stats')
-      .append(markup.comments, markup.likes)
-
-    markup.image
-      .data(data)
-
-
-    $('<div/>', { class: 'p-container-item' })
-      .append(markup.date, markup.image, markup.stats)
-      .appendTo(pongstr.element)
 
     return this
-  };
+  }
 
-  Pongstagram.prototype.modal = function () {
-    var
-      pongstr   = this
-    , options   = this.options
-    , instagram = this.ig_api
+  Pongstagram.prototype.getMedia = function (url) {
+    var that = this
 
-    var close    = $('<button>', { class: 'p-modal-close', text: '&times;' })
-      , backdrop = $('<div/>', { class: 'p-modal-container' })
-      , content  = $('<div/>', { class: 'p-modal-content', append: close })
+    $.get(url, function (data) {
+      if (that.mode == 'profile')
+        console.log(that.mode, data)
 
-    function showModalWindow () {
-      backdrop
-        .append(content.addClass('active'))
-        .addClass('in')
+      if (that.mode == 'tag')
+        console.log(that.mode, data)
 
-      setTimeout(function () {
-        content.addClass('in')
-      }, 200)
-    }
-
-    function hideModalWindow () {
-      content
-        .removeClass('in')
-        .removeAttr('id')
-        .removeClass('active')
-        .empty()
-
-      setTimeout(function () {
-        backdrop.removeClass('in')
-      }, 500)
-
-      setTimeout(function () {
-        backdrop
-          .removeClass('active')
-          .remove()
-      }, 900)
-
-    }
-
-    $(document)
-      .on('pongstr.showmodal', showModalWindow)
-      .on('pongstr.hidemodal', hideModalWindow)
-
-      .on('click', '.p-container-thumbnail', function (e) {
-        $('body').append(backdrop.addClass('active'))
-        $(this).trigger('pongstr.showmodal')
-
-        e.preventDefault()
-      })
-      .on('click', '.p-modal-close', function (e) {
-        $(this).trigger('pongstr.hidemodal')
-
-        e.preventDefault()
-      })
-  };
-
-  Pongstagram.prototype.paginator = function () {
-    var
-      pongstr = this
-    , options = this.options
-    , pgrblck = $('<div/>')
-    , pgrbttn = $('<button/>')
-
-    pgrbttn
-      .html('<span>more</span>')
-      .addClass('p-paginator-button')
-      .end()
-
-    pgrblck
-      .append(pgrbttn)
-      .addClass('p-paginator-block')
-      .insertAfter(pongstr.element)
-      .end()
-
-    pgrbttn.on('click', function (e) {
-      var next = $(pongstr.element).data('nextPage')
-
-      if (!next)
-        pgrbttn.attr('disabled', 'disabled')
-      else
-        pongstr.media(next)
-
-      e.preventDefault()
-    })
-  };
-
-  Pongstagram.prototype.utils = {
-    stringify: function (object) {
-      var urlstring = []
-
-      for (var key in object) {
-        (object.hasOwnProperty(key)) &&
-          urlstring.push(encodeURIComponent(key) + '=' + encodeURIComponent(object[key]))
-      }
-
-      return urlstring.join('&')
-    }
-  };
-
-  Pongstagram.prototype.media = function (endpoint) {
-    var
-      pongstr = this
-    , options = this.options
-
-    function iterateMedia (data) {
-      data.pagination.next_url = !$.isEmptyObject(data.pagination.next_url) ?
-        data.pagination.next_url : null
-
-      $(pongstr.element)
-        .data('nextPage', data.pagination.next_url)
-
-      $.each(data.data, function (a, b) {
-        b.created_time = new Date(b.created_time * 1000).toDateString()
-        pongstr.thumbnail(b)
-      })
-    }
-
-    $.ajax({
-        url:      endpoint
-      , cache:    true
-      , method:   'GET'
-      , dataType: 'jsonp'
-      , success:  function (data) {
-        if (options.show !== 'profile')
-          iterateMedia(data)
-
-      }
-    })
+      if (that.mode == 'gallery')
+        console.log(that.mode, data)
+    }, 'jsonp');
 
     return this
-  };
+  }
 
   Pongstagram.prototype.start = function () {
-    var
-      pongstr   = this
-    , options   = this.options
-    , instagram = this.ig_api
+    var that  = this
+    var media = {}
+    var base  = '/users/'
 
-    var typemedia = {
-      feed: function () {
-        return endpoint('users/self/feed?')
-      },
-      liked: function () {
-        return endpoint('users/self/media/liked?')
-      },
-      recent: function () {
-        return endpoint('users/'+ options.accessId +'/media/recent?')
-      },
-      profile: function () {
-        return endpoint()
-      },
-      tag: function () {
-        return endpoint('tags/' + options.show + '/media/recent?')
-      }
-    };
-
-    function endpoint (endpoint) {
-      if (!endpoint) {
-        instagram += 'users/' + options.accessId
-        instagram += pongstr.utils.stringify({ access_token: options.accessToken })
-      }
-
-      if (endpoint) {
-        instagram += endpoint
-        instagram += pongstr.utils.stringify({
-          count: options.count,
-          access_token: options.accessToken
-        })
-      }
-
-      pongstr.modal()
-      pongstr.paginator()
-      pongstr.media(instagram)
-
-      return
+    // Display user's feed
+    media.feed = function () {
+      that.mode = 'gallery'
+      base += 'self/feed?' + $.param({
+          count: that.options.count
+        , access_token: that.options.accessToken
+      })
+      that.request = that.instgrm + base
+      return that.request
     }
 
-      $(pongstr.element)
-        .data('paginate', options.show)
-        .addClass('p-container-flexbox')
+    // Display user's liked media
+    media.liked = function () {
+      that.mode = 'gallery'
+      base += 'self/media/liked?' + $.param({
+          count: that.options.count
+        , access_token: that.options.accessToken
+      })
+      that.request = that.instgrm + base
+      return that.request
+    }
 
-    typemedia[options.show] && typemedia[options.show]()
+    // Display user's profile
+    media.profile = function () {
+      that.mode = 'profile'
+      base += that.options.accessId + '?'
+      base += $.param({ access_token: that.options.accessToken })
+      that.request = that.instgrm + base
+      return that.request
+    }
 
-    return this
-  };
+    // Display user's recent media
+    media.recent = function () {
+      that.mode = 'gallery'
+      base += that.options.accessId
+      base += '/media/recent?' + $.param({
+          count: that.options.count
+        , access_token: that.options.accessToken
+      })
+      that.request = that.instgrm + base
+      return that.request
+    }
 
-  Pongstagram.prototype.authenticate = function (callback) {
-    var
-      access = (this.options.accessId || '' &&
-                this.options.accessToken || '') ? true : false
+    // Display user Tag
+    media.tag = function () {
+      var tags = that.options.show &&
+        that.options.show.trim().split(' ');
+
+      that.mode = 'tag'
+      tags.forEach(function (e) {
+        var path  = that.instgrm + '/tags/'
+        path += encodeURIComponent(e.replace(/^\#/g,'').trim())
+        path += '/media/recent?' + $.param({
+            count: that.options.count
+          , access_token: that.options.accessToken
+        })
+        that.getMedia(path)
+      })
+      that.request = that.instgrm + base
+      return that.request
+    }
+
+    that.options.show.match(/^\#(\w+|\d+)/g) ?
+      media.tag() : (media[that.options.show] || Object.keys(media).indexOf(that.options.show) !== -1) ?
+        media[that.options.show]() && that.getMedia(that.request) : media.tag();
+
+    return true
+  }
+
+  Pongstagram.prototype.authenticate = function () {
+    var access = (this.options.accessId || '' &&
+                  this.options.accessToken || '') ? true : false;
     if (access)
       this.start(); return true
 
     if (!access)
-      console.log('%cPongstagr.am requires an Instagram Access ID and Token to access your media.','color:red')
-      console.log('%cYou may access public media by using `{show: \'tag-you-like\'}`.','color:green')
+      console.info('%cPongstagr.am requires an Instagram Access ID and Token to access your media.','color:red')
+      console.info('%cYou may access public media by using `{show: \'tag-you-like\'}`.','color:green')
       return false
-  };
-
-  $.fn.lazyload = function (callback) {
-    return this.each(function () {
-      var $image = $(this)
-        ,  start = 0
-
-      if (++start == $image.length)
-        $image.fadeIn()
-        callback && callback()
-    })
-  };
+  }
 
   // Plugin Definition
   $.fn.pongstgrm = function (option) {
-    var options = $.extend({}, $.fn.pongstgrm.defaults, option)
-
+    var opt = $.extend({}, $.fn.pongstgrm.defaults, option)
     return this.each(function () {
-      var pongstagrm = new Pongstagram($(this)[0], options)
-          pongstagrm.authenticate()
+      for (var i = 0; i < arguments.length; i++) {
+        if (typeof arguments[i] === 'function')
+          opt.callback = arguments[i]()
+
+        if (typeof arguments[i] === 'object')
+          opt[arguments[i]]
+      }
+
+      new Pongstagram($(this)[0], opt)
     })
-  };
+  }
 
   // Plugin default Options
-  $.fn.pongstgrm.defaults = Pongstagram.defaults
+  $.fn.pongstgrm.defaults = Pongstagram.DEFAULTS
 
 }(jQuery);
--- tests/test.js
+++ tests/test.js
@@ -15,8 +15,14 @@ $(function () { 'use strict';
     strictEqual($plugin[0], $element[0], 'collection contains element')
   })
 
-  test('should expose default settings', function () {
+  test('should expose default settings and have valid values', function () {
     ok($.fn.pongstgrm.defaults, 'default options are defined')
+    ok(typeof $.fn.pongstgrm.defaults.show === 'string', 'String: Shows what time of media should be displayed.')
+    ok(typeof $.fn.pongstgrm.defaults.likes === 'boolean', 'Boolean: Enable/Disable media "Likes" display.')
+    ok(typeof $.fn.pongstgrm.defaults.count === 'number', 'Number: Shows how many items will be displayed.')
+    ok(typeof $.fn.pongstgrm.defaults.accessId === 'string', 'Access ID must be string')
+    ok(typeof $.fn.pongstgrm.defaults.timestamp === 'boolean', 'Boolean: Enable/Disable media "Timestamp" display.')
+    ok(typeof $.fn.pongstgrm.defaults.accessToken === 'string', 'Access Token must be string')
   })
 
 });
